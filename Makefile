92db998ddefe5e40b51b2bf90d7175a24a40e18d
Marcel <marcel.zielinski@studenci.collegiumwitelona.pl>
2024-10-15 15:37:28 +0200

    docker

---
+++ Makefile
@@ -0,0 +1,22 @@
+init: check-if-env-file-exist
+	@docker compose build
+
+dev:
+	@docker compose up -d
+	@docker compose exec app /dev.sh
+
+stop:
+	@docker compose stop
+
+shell:
+	@docker compose exec -it app bash
+
+
+migrate:
+	@docker compose exec app php artisan migrate:fresh --seed
+
+check-if-env-file-exist:
+	@if [ ! -f ".env" ]; then \
+	  echo ".env file does not exist. Create a .env file and adjust it." ;\
+	  exit 1;\
+	fi; \
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import { networkInterfaces } from 'os'
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
-        }),
-        react(),
-    ],
-});
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: 'resources/js/app.jsx',
+                refresh: true,
+            }),
+            react(),
+        ],
+        server: {
+            hmr: {
+                host: 'localhost',
+            },
+            host: true,
+            port: env.VITE_PORT,
+        },
+    })
+}
