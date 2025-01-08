c4ba52197da0fb4ef5e5a136f30e460ac0eab61a
Oliwia Charyszczak <oliwia.charyszczak@studenci.collegiumwitelona.pl>
2025-01-08 21:36:42 +0100

    Add delete vehicle functionality

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -3,8 +3,9 @@ import { Head } from '@inertiajs/react';
 import DeleteUserForm from './Partials/DeleteUserForm';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
+import DeleteVehicleForm from "@/Pages/Profile/Partials/DeleteVehicleForm.jsx";
 
-export default function Edit({ mustVerifyEmail, status }) {
+export default function Edit({ mustVerifyEmail, vehicles, status }) {
     return (
         <AuthenticatedLayout
             header={
@@ -26,11 +27,15 @@ export default function Edit({ mustVerifyEmail, status }) {
                     </div>
 
                     <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
-                        <UpdatePasswordForm className="max-w-xl" />
+                        <UpdatePasswordForm className="max-w-xl"/>
                     </div>
 
                     <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
-                        <DeleteUserForm className="max-w-xl" />
+                        <DeleteVehicleForm className="max-w-xl" vehicles={vehicles}/>
+                    </div>
+
+                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
+                        <DeleteUserForm className="max-w-xl"/>
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\UserController;
 
 Route::get('/user/{username}', [ProfileController::class, 'show']);
 
@@ -26,5 +27,23 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/user/{id}', [UserController::class, 'getUser'])->name('getUser');
+Route::post('/user', [UserController::class, 'createUser'])->name('createUser');
+
+
+Route::get('/get/spendings/{vehicle_id}', [\App\Http\Controllers\SpendingController::class, 'getSpendings'])->name('getSpendings')->middleware('auth.basic');
+Route::get('/get/spending/{id}/{vehicle_id}', [\App\Http\Controllers\SpendingController::class, 'getSpending'])->name('getSpending')->middleware('auth.basic');
+Route::post('/create/spending/{vehicle_id}', [\App\Http\Controllers\SpendingController::class, 'createSpending'])->name('createSpending')->middleware('auth.basic');
+Route::post('/edit/spending/{id}/{vehicle_id}', [\App\Http\Controllers\SpendingController::class, 'editSpending'])->name('editSpending')->middleware('auth.basic');
+Route::delete('/deleteuser/spending/{id}/{vehicle_id}', [\App\Http\Controllers\SpendingController::class, 'deleteSpending'])->name('deleteSpending')->middleware('auth.basic');
+
+
+Route::get('/getuser/vehicle/{id}', [\App\Http\Controllers\VehicleController::class, 'getVehicle'])->name('getVehicle')->middleware('auth.basic');
+Route::get('/getuser/vehicles', [\App\Http\Controllers\VehicleController::class, 'getVehicles'])->name('getVehicles')->middleware('auth.basic');
+Route::post('/create/vehicle', [\App\Http\Controllers\VehicleController::class, 'createVehicle'])->name('createVehicle')->middleware('auth.basic');
+Route::post('/edit/vehicle/{id}', [\App\Http\Controllers\VehicleController::class, 'editVehicle'])->name('editVehicle')->middleware('auth.basic');
+Route::delete('/deleteuser/vehicle/{id}', [\App\Http\Controllers\VehicleController::class, 'deleteVehicle'])->name('deleteVehicle')->middleware('auth.basic');
+
 
 require __DIR__.'/auth.php';
