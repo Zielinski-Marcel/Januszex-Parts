49875762f54c0de8cfb3427a4425df2a623f4af6
Oliwia Charyszczak <oliwia.charyszczak@studenci.collegiumwitelona.pl>
2025-01-11 01:31:20 +0100

    Add input errors to spending form

--- resources/js/Pages/Vehicle/AddSpending.jsx
+++ resources/js/Pages/Vehicle/AddSpending.jsx
@@ -1,5 +1,6 @@
 import {Head, useForm} from "@inertiajs/react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.jsx";
+import InputError from "@/Components/InputError.jsx";
 
 export default function AddSpending({vehicle}){
     const form = useForm();
@@ -26,7 +27,9 @@ export default function AddSpending({vehicle}){
                                             value={form.data.price}
                                             onChange={(e) => form.setData("price", e.target.value)}
                                             className="mt-1 w-full block rounded-md border-gray-300 shadow-sm focus:border-primary focus:ring-primary"
+
                                         />
+                                        <InputError message={form.errors.price} className="mt-1" />
                                         <input
                                             type="text"
                                             name="type"
@@ -35,6 +38,7 @@ export default function AddSpending({vehicle}){
                                             onChange={(e) => form.setData("type", e.target.value)}
                                             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-emerald-500 focus:ring-primary"
                                         />
+                                        <InputError message={form.errors.type} className="mt-1" />
                                         <input
                                             type="date"
                                             name="date"
@@ -43,6 +47,7 @@ export default function AddSpending({vehicle}){
                                             onChange={(e) => form.setData("date", e.target.value)}
                                             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-emerald-500 focus:ring-primary"
                                         />
+                                        <InputError message={form.errors.date} className="mt-1" />
                                         <input
                                             type="text"
                                             name="place"
@@ -51,6 +56,7 @@ export default function AddSpending({vehicle}){
                                             onChange={(e) => form.setData("place", e.target.value)}
                                             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-emerald-500 focus:ring-primary"
                                         />
+                                        <InputError message={form.errors.place} className="mt-1" />
                                         <input
                                             type="text"
                                             name="description"
@@ -59,6 +65,7 @@ export default function AddSpending({vehicle}){
                                             onChange={(e) => form.setData("description", e.target.value)}
                                             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-emerald-500 focus:ring-primary"
                                         />
+                                        <InputError message={form.errors.description} className="mt-1" />
                                         <button
                                             type="submit"
                                             className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
