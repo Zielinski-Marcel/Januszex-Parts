6ebf9be3589a482b2158ec7cd5203ee72ba69bdf
Marcel <marcel.zielinski@studenci.collegiumwitelona.pl>
2025-01-19 21:28:58 +0100

    logs

--- app/Http/Controllers/SpendingController.php
+++ app/Http/Controllers/SpendingController.php
@@ -20,7 +20,10 @@ class SpendingController extends Controller
         if (!$vehicle) {
             return response()->json(['message' => 'Vehicle not found or does not belong to the authenticated user.'], 404);
         }
-
+        activity()
+            ->causedBy($user)
+            ->withProperties(['vehicle_id' => $vehicle->id])
+            ->log('Viewed all spendings for vehicle.');
         $spendings = $vehicle->spendings;
         return response()->json(['spending'=>$spendings]);
     }
@@ -37,10 +40,21 @@ class SpendingController extends Controller
         if (!$spending) {
             return response()->json(['message' => 'Spending not found or does not belong to the authenticated user.'], 404);
         }
+        activity()
+            ->causedBy($user)
+            ->withProperties([
+                'spending_id' => $spending->id,
+                'vehicle_id' => $vehicle->id
+            ])
+            ->log('Viewed a specific spending.');
         return response()->json(['spending'=>$spending]);
     }
 
     public function create(Vehicle $vehicle){
+        activity()
+            ->causedBy(auth()->user())
+            ->withProperties(['vehicle_id' => $vehicle->id])
+            ->log('Accessed page to create a new spending.');
         return Inertia::render('Vehicle/AddSpending', ['vehicle' => $vehicle]);
     }
 
@@ -58,13 +72,28 @@ class SpendingController extends Controller
         $validated = $request->validated();
         $validated['user_id'] = auth()->id();
         $validated['vehicle_id'] = $vehicle['id'];
-        Spending::create($validated);
+        $spending=Spending::create($validated);
 
-        // Return a JSON response
+        activity()
+            ->causedBy($user)
+            ->withProperties([
+                'spending_id' => $spending->id,
+                'vehicle_id' => $vehicle->id,
+                'amount' => $spending->amount,
+                'description' => $spending->description
+            ])
+            ->log('Created a new spending.');
         return redirect()->to("/dashboard/" . $vehicle_id);
     }
 
     public function edit(Spending $spending){
+        activity()
+            ->causedBy(auth()->user())
+            ->withProperties([
+                'spending_id' => $spending->id,
+                'vehicle_id' => $spending->vehicle_id
+            ])
+            ->log('Accessed page to edit a spending.');
         return Inertia::render('Vehicle/EditSpending', ['spending' => $spending]);
     }
 
@@ -81,7 +110,15 @@ class SpendingController extends Controller
 
         $validated = $request->validated();
         $spending->update($validated);
-
+        activity()
+            ->causedBy($user)
+            ->withProperties([
+                'spending_id' => $spending->id,
+                'vehicle_id' => $spending->vehicle_id,
+                'amount' => $spending->amount,
+                'description' => $spending->description
+            ])
+            ->log('Updated a spending.');
         return redirect()->to("/dashboard/" . $spending->vehicle_id);
     }
     public function deleteSpending(Spending $spending): \Illuminate\Http\RedirectResponse
@@ -96,6 +133,13 @@ class SpendingController extends Controller
         }
 
         $spending->delete();
+        activity()
+            ->causedBy($user)
+            ->withProperties([
+                'spending_id' => $spending->id,
+                'vehicle_id' => $spending->vehicle_id
+            ])
+            ->log('Deleted a spending.');
         return redirect()->back()->with("status", 'Spending deleted successfully.');
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,12 +12,4 @@ class UserController extends Controller
         $user = User::where('id', $id)->first();
         return response()->json(['user'=>$user]);
     }
-    public function createUser(): JsonResponse{
-        return response()->json([]);
-    }
-    public function changeUserName(User $user): JsonResponse{
-        $user->name='jd';
-        $user->save();
-        return response()->json(['username'=>$user->name])->setStatusCode(200);
-    }
 }
