4c20ab4ba25014ab51d44b3facdbbd01cf3fe280
Dawid <dawid.buchci@gmail.com>
2025-01-19 21:54:52 +0100

    change return from blade to jsx

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
     public function index(){
+        $admin = auth()->user();
+        $users = User::query()->where("is_admin", false)->withCount("vehicles")->get();
 
-
-        return view('admin.dashboard.blade.php');
+        return Inertia::render(
+            'Admin/Dashboard',
+            ["admin" => $admin, "users" => $users]
+        );
     }
 }
